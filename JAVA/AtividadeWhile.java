095c708a02092b88cd7e52cdc6a8086fe527578e
kelvyn777 <kelvynsabino10@gmail.com>
2022-08-09 15:02:32 -0300

    ATIVIDADES

--- JAVA/AtividadeWhile.java
+++ JAVA/AtividadeWhile.java
@@ -7,16 +7,15 @@ public class AtividadeWhile {
 	public static void main(String[] args) {
 		   /*Solicitar a idade de várias pessoas e imprimir: Total de pessoas com menos de
 	        21 anos. Total de pessoas com mais de 50 anos. O programa termina quando
-	        idade for =-99. (WHILE)
-	        Pode usar literalmente em qualquer lugar, inclusive, por nosso powerbank ser a prova de agua
-	        Sera feito porque vendo as necessidades do dia a dia, percebemos que carregar seu telefone*/
+	        idade for =-99. (WHILE)*/
+	     
 
 	        int idade = 0, menos21=0, mais50=0;
-	        Scanner input = new Scanner(System.in);
-	        while(idade !=-99) {
+	        Scanner leia = new Scanner(System.in);
+	        while(idade <=99) {
 	            System.out.println("Entre com a sua idade: ");
-	            idade = input.nextInt();
-	            if(idade<21 && idade != -99) {
+	            idade = leia.nextInt();
+	            if(idade<21 && idade <=99) {
 	                menos21++;
 	            }else if(idade >50) {
 	                mais50++;
